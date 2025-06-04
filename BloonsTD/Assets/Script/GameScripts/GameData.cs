266e752346d8e140cba2e8325efb1599d0f5620f
alext218973 <alextrudgill@hotmail.com>
2025-06-04 10:45:12 -0400

    Shop menu
    Added many elements to the shop menu. Menu will be updated with many more icons and elements

---
+++ BloonsTD/Assets/Script/GameScripts/GameData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameData
+{
+    public int Rounds;
+    public int Money;
+    public int Lives;
+
+    public GameData(int rounds, int money, int lives)
+    {
+        this.Rounds = rounds;
+        this.Money = money;
+        this.Lives = lives;
+    }
+
+    //methods to change values
+}
