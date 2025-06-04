0a41b72bd0fc6f36ae455d663df001f0c3db6cd8
alext218973 <alextrudgill@hotmail.com>
2025-06-03 22:35:42 -0400

    Working music player
    2 different music themes that play across all scenes. Mute music toggle works and saves across scenes. Button and game sounds will be added later and use the same conditions as this mute music system.

--- BloonsTD/Assets/Script/Audio.cs
+++ BloonsTD/Assets/Script/Audio.cs
@@ -1,18 +1,55 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Audio : MonoBehaviour
 {
     public static bool MuteMusic = false;
-
     public static bool MuteSound = false;
+
+    private static Audio instance;
+
+    private AudioSource audioSource;
+    private string currentClipName = "";
+
     void Start()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject); 
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = true;  // loop on start
+    }
+
+    public void PlayMusic(string clipName)
+    {
+        if (clipName == currentClipName)
+            return;  // Already playing so do nothing
+
+        AudioClip clip = Resources.Load<AudioClip>(clipName);
+
+        audioSource.clip = clip;
+        currentClipName = clipName;
+        audioSource.Play();
     }
 
     void Update()
     {
-        
+        audioSource.mute = MuteMusic;
+
+        if (MuteMusic == false && SceneManager.GetActiveScene().name == "Gameplay")
+        {
+            PlayMusic("GameTheme");
+        }
+        else
+        {
+            PlayMusic("MainMenuTheme");
+        }
     }
 }
