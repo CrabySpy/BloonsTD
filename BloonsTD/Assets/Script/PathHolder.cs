76c4352305987b5b425aa0c1a76c84b436e9f30a
steven <steven.wong26@ycdsbk12.ca>
2025-06-06 23:05:18 -0400

    WayPoints fixed
    way points broke like for the billionth time so i had to fix it...again (also my edits are not saving and committing like my progress keeps getting erased and its super annoying)

--- BloonsTD/Assets/Script/PathHolder.cs
+++ BloonsTD/Assets/Script/PathHolder.cs
@@ -1,24 +1,24 @@
-//using UnityEngine;
+using UnityEngine;
 
-//public class PathHolder : MonoBehaviour
-//{
-//    public Transform[] waypoints; // these are waypoints that define the balloon's path (put them in order!!)
+public class PathHolder : MonoBehaviour
+{
+    public Transform[] waypoints;
 
-//    public int Count => waypoints.Length;
+    public int Count => waypoints.Length;
 
-//    public Transform GetWaypoint(int index)
-//    {
-//        return waypoints[index];
-//    }
+    public Transform GetWaypoint(int index)
+    {
+        return waypoints[index];
+    }
 
-//    private void OnDrawGizmos()
-//    {
-//        if (waypoints == null || waypoints.Length < 2) return;
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null || waypoints.Length < 2) return;
 
-//        Gizmos.color = Color.green; // visual path that'll show where the balloons will travel. delete this after everything has been set up kthx
-//        for (int i = 0; i < waypoints.Length - 1; i++)
-//        {
-//            Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
-//        }
-//    }
-//}
+        Gizmos.color = Color.green;
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+        }
+    }
+}
