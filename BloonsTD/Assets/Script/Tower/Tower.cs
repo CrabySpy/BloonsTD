71ede94d0763af0b68a4a2bc3370f0de9b0f8245
Will Hui <willhui0414@gmail.com>
2025-06-08 14:13:07 -0400

    fixed text off scale bug

--- BloonsTD/Assets/Script/Tower/Tower.cs
+++ BloonsTD/Assets/Script/Tower/Tower.cs
@@ -1,18 +1,63 @@
 using UnityEngine;
 using UnityEditor;
+
 public abstract class Tower : MonoBehaviour
 {
     public TowerInfo towerInfo;
     [SerializeField] private Transform rotationalPoint;
+    [SerializeField] private LayerMask bloonsMask;
 
+    private Transform target;
 
-    public void OnDrawGizmosSelected()
+    private void Update()
     {
-        Handles.color = Color.white;
-        Handles.DrawWireDisc(transform.position, transform.forward, towerInfo.Range);
-        
+        if (target == null)
+        {
+            FindTarget();
+        }
+        else
+        {
+            if (Vector2.Distance(transform.position, target.position) > towerInfo.Range)
+            {
+                target = null; // Lose target if out of range
+                return;
+            }
+
+            RotateTowardsTarget();
+            Attack();
+        }
     }
 
+    private void FindTarget()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, towerInfo.Range, bloonsMask);
+
+        foreach (Collider2D hit in hits)
+        {
+            target = hit.transform;
+            break; // Just pick the first one
+        }
+    }
+
+    public void RotateTowardsTarget()
+    {
+        if (target == null) return;
+
+        Vector2 direction = target.position - rotationalPoint.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+
+        rotationalPoint.rotation = Quaternion.Slerp(rotationalPoint.rotation, targetRotation, 3 * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (towerInfo != null)
+        {
+            Handles.color = Color.white;
+            Handles.DrawWireDisc(transform.position, Vector3.forward, towerInfo.Range);
+        }
+    }
 
     public abstract void Attack();
 }
