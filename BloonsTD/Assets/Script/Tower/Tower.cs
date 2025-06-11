2febda52f4bcb8523371268eaee9933867ea2acd
Will Hui <willhui0414@gmail.com>
2025-06-10 21:56:19 -0400

    Fixed bomb tower range indicator bug

--- BloonsTD/Assets/Script/Tower/Tower.cs
+++ BloonsTD/Assets/Script/Tower/Tower.cs
@@ -8,7 +8,7 @@ public abstract class Tower : MonoBehaviour
     [SerializeField] private LayerMask bloonsMask;
     [SerializeField] protected Transform firePoint;
 
-    [SerializeField] private GameObject rangeCircle;
+    private GameObject rangeCircle;
     private static Tower currentlySelectedTower = null;
 
 
@@ -32,6 +32,12 @@ public abstract class Tower : MonoBehaviour
 
     protected virtual void Start()
     {
+        SetAttackInterval();
+        CreateRangeCircle();
+    }
+
+    private void SetAttackInterval()
+    {
         if (towerInfo.SpeedString == "Slow")
         {
             attackInterval = 1f;
@@ -49,15 +55,29 @@ public abstract class Tower : MonoBehaviour
             attackInterval = 0.1f;
         }
         
-        if (rangeCircle != null && towerInfo != null)
-        {
-        // Because default sprite size = 1 unit radius, so scale to match towerInfo.Range:
-        float diameter = towerInfo.Range * 2f;
-        rangeCircle.transform.localScale = new Vector3(diameter, diameter, 1f);
+    }
 
-        // Make sure it starts hidden
+    private void CreateRangeCircle()
+    {
+        rangeCircle = new GameObject("RangeCircle");
+
+        // Make it not a child of the scaled tower, but still move with it
+        rangeCircle.transform.position = transform.position;
+        rangeCircle.transform.rotation = Quaternion.identity;
+
+        var sr = rangeCircle.AddComponent<SpriteRenderer>();
+        sr.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Knob.psd");
+        sr.color = new Color(225f / 255f, 225f / 255f, 225f / 255f, 0.2f); // Semi-transparent white
+        sr.sortingLayerName = "Towers";
+        sr.sortingOrder = 1;
+
+        // Calculate accurate world scale
+        float spriteDiameterUnits = sr.sprite.bounds.size.x;
+        float desiredDiameter = towerInfo.Range * 2f;
+        float scaleFactor = desiredDiameter / spriteDiameterUnits;
+
+        rangeCircle.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
         rangeCircle.SetActive(false);
-        }
     }
 
     protected void Update()
@@ -92,6 +112,11 @@ public abstract class Tower : MonoBehaviour
                 animator?.SetBool(ATTACK_STRING, isAttacking); // STOP animating
             }
             // Debug.Log($"{gameObject.name}: Mouse out of range.");
+        }
+
+        if (rangeCircle != null)
+        {
+            rangeCircle.transform.position = transform.position;
         }
     }
 
@@ -165,12 +190,14 @@ public abstract class Tower : MonoBehaviour
 
     private void ShowRange()
     {
+        Debug.Log($"{gameObject.name}: Showing range circle.");
         if (rangeCircle != null)
             rangeCircle.SetActive(true);
     }
 
     private void HideRange()
     {
+        Debug.Log($"{gameObject.name}: Hiding range circle.");
         if (rangeCircle != null)
             rangeCircle.SetActive(false);
     }
@@ -185,6 +212,5 @@ public abstract class Tower : MonoBehaviour
     }
 
     public abstract void Attack();
-
-    // public abstract void AnimationAttack();
 }
+    // public abstract void AnimationAttack();
