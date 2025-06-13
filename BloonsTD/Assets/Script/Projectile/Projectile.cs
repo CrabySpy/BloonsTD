e1723e8bd801d3f0df0604b38e4c32daaf2810a6
Will Hui <willhui0414@gmail.com>
2025-06-12 20:14:47 -0400

    fixed projectile not working bug

--- BloonsTD/Assets/Script/Projectile/Projectile.cs
+++ BloonsTD/Assets/Script/Projectile/Projectile.cs
@@ -11,20 +11,29 @@ public class Projectile : MonoBehaviour
     }
 
     protected virtual void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("PowerUp"))
     {
-        other.GetComponent<PowerUpCrate>().ActivatePowerUp();
-        Destroy(gameObject);
-    }
+        if (other.CompareTag("PowerUp"))
+        {
+            other.GetComponent<PowerUpCrate>().ActivatePowerUp();
+            Destroy(gameObject);
+        }
 
-    if (other.CompareTag("Bloon"))
-    {
-        other.GetComponent<Bloon>().TakeDamage(1);
-        Destroy(gameObject);
-    }
+        if (other.CompareTag("Bloon"))
+        {
+            other.GetComponent<Bloon>().TakeDamage(1);
+            Destroy(gameObject);
+        }
 
-}
+        Bloon bloon = other.GetComponent<Bloon>();
+        if (bloon != null)
+        {
+            bloon.TakeDamage(damage);
+            Destroy(gameObject);
+            Debug.Log("Projectile hit a bloon: " + bloon.name);
+        }
+
+    }
+    
 
 
     public void OnBecameInvisible()
